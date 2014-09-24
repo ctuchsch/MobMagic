daa0fc1bd640a7af6c19f6939fd6c1f23ebafa88
Jared Darr <jdarr08@gmail.com>
2014-09-24 17:00:34 -0400

    Moved ItemLinkCore.java into correct place
    Somehow the ItemLinkingCore class file was in the textures package.
    Moved it into the correct place and updated import. Also updated
    Starshine Ore texture

---
+++ Java/ctuchsch/mods/mobmagic/items/ItemLinkingCore.java
@@ -0,0 +1,12 @@
+package ctuchsch.mods.mobmagic.items;
+
+import ctuchsch.mods.mobmagic.MobMagic;
+import net.minecraft.item.Item;
+
+public class ItemLinkingCore extends Item {
+	
+	public ItemLinkingCore() {
+		super();
+		this.setCreativeTab(MobMagic.tabCustom);
+	}
+}
